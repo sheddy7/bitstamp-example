f05cb843a7dcb2fa97aab93d9dd3f886b3182a25
shedblake7 <spblake7@gmail.com>
2018-03-29 14:24:15 +0100

    Extract enzyme configuration to separate file

--- test/unit/client/components/BitstampDataViewer.test.js
+++ test/unit/client/components/BitstampDataViewer.test.js
@@ -1,8 +1,3 @@
-const Enzyme = require('enzyme');
-const EnzymeAdapter = require('enzyme-adapter-react-16');
-
-// Setup enzyme's react adapter
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 import React from 'react';
 import { render } from 'enzyme';
